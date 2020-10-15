9c09bb4af8fcbe3a8f75b1e561a123ff08eae186
JinminXu <jimmyxu200011@gmail.com>
2020-10-16 01:07:40 +0800

    Add more js feature (choose file and display chosen file's name function)

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ function closeNav() {
 	document.getElementById("nav").style.width = "0";
 	document.getElementById("open").style.display = "block";
 }
+
+const Filebtn = document.getElementById("pubfile");
+const btn = document.getElementById("pubbtn");
+const Filetxt = document.getElementById("pubtext");
+
+btn.addEventListener("click", function(){
+		Filebtn.click();
+	});
+
+Filebtn.addEventListener("change", function(){
+	if(Filebtn.value){
+		Filetxt.innerHTML = Filebtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+			}else{
+				Filetxt.innerHTML  ="No file chosen";
+				}
+		});
